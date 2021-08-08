6b6a996d42b10e8fb9cbcaaf0f6ed3e8ca72de97
scarter31191 <scarter31191@gmaile.com>
2021-08-08 08:45:40 -0400

    added button colums

--- src/App.js
+++ src/App.js
@@ -24,12 +24,27 @@ class App extends Component {
   render() {
     const buttons = [
       {symbol: 'C', cols: 3, action: this.reset},
-      {symbol: '/', cols: 1, action: this.addToCurrent}
+      {symbol: '/', cols: 1, action: this.addToCurrent},
+      {symbol: '7', cols: 1, action: this.addToCurrent},
+      {symbol: '8', cols: 1, action: this.addToCurrent},
+      {symbol: '9', cols: 1, action: this.addToCurrent},
+      {symbol: 'x', cols: 1, action: this.addToCurrent},
+      {symbol: '4', cols: 1, action: this.addToCurrent},
+      {symbol: '5', cols: 1, action: this.addToCurrent},
+      {symbol: '6', cols: 1, action: this.addToCurrent},
+      {symbol: '-', cols: 1, action: this.addToCurrent},
+      {symbol: '1', cols: 1, action: this.addToCurrent},
+      {symbol: '2', cols: 1, action: this.addToCurrent},
+      {symbol: '3', cols: 1, action: this.addToCurrent},
+      {symbol: '+', cols: 1, action: this.addToCurrent},
+      {symbol: '0', cols: 2, action: this.addToCurrent},
+      {symbol: '.', cols: 1, action: this.addToCurrent},
+      {symbol: '=', cols: 1, action: this.addToCurrent}
     ]
     return (
       <div className="App">
         <input className="result" type="text" value={this.state.current} />
-        <Button />
+        
       </div>
     );
   }
