b537e5b05692b091861dd736916193635edfe315
scarter31191 <scarter31191@gmaile.com>
2021-08-08 08:51:58 -0400

    added list of button to the DOM

--- src/App.js
+++ src/App.js
@@ -44,7 +44,9 @@ class App extends Component {
     return (
       <div className="App">
         <input className="result" type="text" value={this.state.current} />
-        
+        {buttons.map((btn, i) => {
+          return <Button symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action} />
+        })}
       </div>
     );
   }
