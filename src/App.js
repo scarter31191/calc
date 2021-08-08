a74e0d14f571c5bcf1a27b1780976382bfd0e476
scarter31191 <scarter31191@gmaile.com>
2021-08-07 23:08:12 -0400

    added button structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React, {Component} from "react";
+import Button from "./components/Button"
 
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
-      current: "",
+      current: '0',
       previous: []
     }
   }
-  
+
+  reset = () => {
+    this.setState({result: '0'});
+  }
+
+  addToCurrent = (symbol) => {
+    this.setState({current: this.state.current + symbol});
+  }
+
   render() {
+    const buttons = [
+      {symbol: 'C', cols: 3, action: this.reset},
+      {symbol: '/', cols: 1, action: this.addToCurrent}
+    ]
     return (
       <div className="App">
         <input className="result" type="text" value={this.state.current} />
+        <Button />
       </div>
     );
   }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+    return (
+        <div>
+            <button className="calc-button">1</button>
+        </div>
+    )
+}
+
+export default Button
